e06599c7f899ad07797615c7755af945101dcc01
Uday Sharma <sharmauday1999@gmail.com>
2022-01-25 13:28:01 -0500

    Functionality Complete

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 # bot.py
 import os
-from db import addUser
+import db
 import discord
 from dotenv import load_dotenv
+from tabulate import tabulate
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -24,23 +25,52 @@ async def on_ready():
     )
 
 
-
 @client.event
 async def on_member_join(member):
-    addUser(member.id)
+    db.addUser(member.id)
 
 @client.event
 async def on_message(message):
     up = '\N{THUMBS UP SIGN}'
     down = '\N{THUMBS DOWN SIGN}'
-
-    print(message.channel.id)
     
     if message.channel.id == 836009066911629312 and len(message.attachments) > 0:
+        db.createNewImage(message)
         await message.add_reaction(up)
         await message.add_reaction(down)
         await message.channel.send("JUDGEMENT TIME " + str(message.author.name))
 
+    if message.content == "!MEME score":
+        currScores = db.getCurrentScores()
 
+        table = [['Name', 'Score']]
+        results = []        
+
+
+        for each in currScores:
+            user = await client.fetch_user(int(each['discord_id']))
+            results.append([user.display_name, each['score']])
+        
+        results.sort(key = lambda x:x[1], reverse = True)
+
+        table = table + results
+
+        await message.channel.send("```" + tabulate(table, headers = 'firstrow') + "```")
+
+@client.event
+async def on_raw_reaction_add(payload):
+    if str(payload.emoji) == '👍':
+        db.addScore(payload.message_id)
+    elif str(payload.emoji) == '👎':
+        db.removeScore(payload.message_id)
+
+@client.event
+async def on_raw_reaction_remove(payload):
+    if str(payload.emoji) == '👍':
+        db.removeScore(payload.message_id)
+    elif str(payload.emoji) == '👎':
+        db.addScore(payload.message_id)
+
+    
 client.run(TOKEN)
 
